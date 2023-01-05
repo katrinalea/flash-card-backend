7b57fa56b2da3228552db2827229b72ce5fb37d9
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-05 18:00:55 +0000

    added post

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Client } from "pg"; 
+import { Client } from "pg";
 
 dotenv.config();
 const client = new Client({ connectionString: process.env.DATABASE_URL });
@@ -30,8 +30,24 @@ app.get("/names", async (req, res) => {
   });
 });
 
+app.post("/names", async (req, res) => {
+  const { name, correct, testamount } = req.body;
+  try {
+    const text =
+      "insert into leaderboard (name, correct, testamount) values ($1, $2, $3) returning *";
+    const values = [name, correct, testamount];
+    const dbResponse = await client.query(text, values);
+    res.status(200).json({
+      status: "success",
+      data: dbResponse.rows,
+    });
+  } catch (err) {
+    console.log(err, "error");
+  }
+});
+
+//add a post request so that when the submit score utton is pressed, there score and name adds to the db
 
 app.listen(PORT_NUMBER, () => {
-    console.log(`Server is listening on port ${PORT_NUMBER}!`);
-  });
-  
+  console.log(`Server is listening on port ${PORT_NUMBER}!`);
+});
